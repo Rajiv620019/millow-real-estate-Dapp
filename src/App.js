a03ac40790f499a1f44cf844915a63f7465f1ff6
Rajiv S <rajiv620019@gmail.com>
2023-03-12 13:07:18 +0530

    Refactored the code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
-
   return (
     <div>
-
-      <div className='cards__section'>
-
-        <h3>Welcome to Millow</h3>
-
+      <div className="cards__section">
+        <h3>Welcome to Millow!</h3>
       </div>
-
     </div>
   );
 }
