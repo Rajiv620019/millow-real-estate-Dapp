550d7e12c2d2f4a4731c5270b4a5c2cecf655ff7
Rajiv S <rajiv620019@gmail.com>
2023-03-15 22:29:13 +0530

    Buy property frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,41 @@ const Home = ({ home, provider, escrow, togglePop }) => {
         <div className="home__image">
           <img src={home.image} alt="Home" />
         </div>
+
+        <div className="home__overview">
+          <h1>{home.name}</h1>
+          <p>
+            <strong>{home.attributes[2].value}</strong> Bedrooms |
+            <strong>{home.attributes[3].value}</strong> Bathrooms |
+            <strong>{home.attributes[4].value}</strong> sqft
+          </p>
+          <p>{home.address}</p>
+          <h2>{home.attributes[0].value} ETH</h2>
+        </div>
+
+        <div>
+          <button className="home__buy">Buy</button>
+        </div>
+        <button className="home__contact">Contact Agent</button>
+
+        <hr />
+
+        <h2>Overview</h2>
+
+        <p>{home.description}</p>
+
+        <hr />
+
+        <h2>Facts and features</h2>
+
+        <ul>
+          {home.attributes.map((attribute, index) => (
+            <li key={index}>
+              <strong>{attribute.trait_tpye}</strong>: {attribute.value}
+            </li>
+          ))}
+        </ul>
+
         <button onClick={togglePop} className="home__close">
           <img src={close} alt="Close" />
         </button>
